c1c5cb74b30bcb9e02106d0a83e96df3c7fe00ff
srijataS <120158965+srijataS@users.noreply.github.com>
2024-05-01 18:10:23 -0400

    Update sentence.py

--- sentence.py
+++ sentence.py
@@ -8,12 +8,3 @@ def load_template():
 def generate_sentence(question, template_data):
     query_text = template_data[question]
     return query_text
-
-#human - computer
-def load_question_answer():
-    with open('json/human_computer_template.json', 'r') as json_file:
-        return json.load(json_file)
-
-def generate_question_answer(question, template_sentence):
-    query_text = template_sentence[question]
-    return query_text
